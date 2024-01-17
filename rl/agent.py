06ea39145c950123a9dd2df43275e701c81c4954
David <zdavid1@myges.fr>
2024-01-17 13:50:03 +0100

    interval bullet

--- rl/agent.py
+++ rl/agent.py
@@ -9,7 +9,10 @@ from game_environment.indicator_bar import IndicatorBar
 class Agent:
     def __init__(self, window, env):
         self.agent_sprite = None
+        self.bullet_sprite = None
         self.agent = None
+        self.bullet = None
+        self.total_time = 0.0
         self.env = env
         self.bar_list = None
         self.indicator_bar = None
@@ -19,6 +22,8 @@ class Agent:
 
     def setup(self):
         self.agent = arcade.SpriteList()
+        self.total_time = 0.0
+        self.bullet = arcade.SpriteList()
         self.bar_list = arcade.SpriteList()
         self.indicator_bar = IndicatorBar(
             owner=self,
@@ -42,6 +47,12 @@ class Agent:
             print("Error: No suitable cells in the environment")
 
         self.agent.append(self.agent_sprite)
+        self.add_bullet()
+
+    def add_bullet(self):
+        self.bullet_sprite = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING * 5)
+        self.bullet_sprite.center_x, self.bullet_sprite.center_y = self.agent_sprite.center_x, self.agent_sprite.center_y
+        self.bullet.append(self.bullet_sprite)
 
     def is_empty_and_adjacent(self, state):
         adjacent_states = [
@@ -65,10 +76,22 @@ class Agent:
 
     def on_draw(self):
         self.agent.draw()
+        self.bullet.draw()
         self.indicator_bar.bar_list.draw()
 
     def update(self, delta_time):
         self.indicator_bar.position = self.agent_sprite.center_x, self.agent_sprite.center_y + 25
+        # the bullet move to the end of the map and then disappear
+        if self.bullet_sprite.center_x > self.window.width or self.bullet_sprite.center_y > self.window.height or \
+                self.bullet_sprite.center_x < 0 or self.bullet_sprite.center_y < 0:
+            self.bullet_sprite.remove_from_sprite_lists()
+        self.total_time += delta_time
+        if self.total_time > RELOAD_BULLET_TIME:
+            self.add_bullet()
+            self.total_time = 0.0
+
+        for bullet in self.bullet:
+            bullet.center_x += BULLET_SPEED
 
     def state_to_xy(self, state):
         return (state[1] + 0.5) * SPRITE_SIZE, \
