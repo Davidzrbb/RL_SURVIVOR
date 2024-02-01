fe0143ea4cd3fee5428b775e0fbb2272cbf39f99
David <zdavid1@myges.fr>
2024-02-01 16:56:06 +0100

    colision bullet to enemy
            #on check si le bullet touche un ennemi avec la hitbox
            #si oui on kill le bullet et l'ennemi et on ajoute l'id de l'ennemi dans la liste des ennemis tués
            #on ne verifie plus par rapport à leur position pour kill ou non car pas assez précis

--- RL_SURVIVOR_REBORN/colision_manager.py
+++ RL_SURVIVOR_REBORN/colision_manager.py
@@ -1,5 +1,7 @@
 import copy
 
+import arcade
+
 from constants import *
 
 
@@ -12,24 +14,19 @@ class CollisionManager:
         pass
 
     def update(self, bullet, enemy):
+        #on check si le bullet touche un ennemi avec la hitbox
+        #si oui on kill le bullet et l'ennemi et on ajoute l'id de l'ennemi dans la liste des ennemis tués
+        #on ne verifie plus par rapport à leur position pour kill ou non car pas assez précis
         for bullet_id in copy.copy(bullet.bullet_id_to_pos):
-            state = bullet.bullet_id_to_pos[bullet_id]
+            check_hitbox_bullet = arcade.check_for_collision_with_list(bullet.bullet_id_to_sprite[bullet_id],
+                                                                       enemy.enemy_sprite_list)
+            if check_hitbox_bullet:
+                enemy_sprite_index = enemy.enemy_sprite_list.index(check_hitbox_bullet[0])
+                id_enemy = list(enemy.enemy_id_to_pos.keys())[enemy_sprite_index]
+                if id_enemy not in enemy.enemy_id_removed:
+                    enemy.enemy_id_removed.append(id_enemy)
+                    enemy.enemy_sprite_list[enemy_sprite_index].kill()
 
-            # Find the key in enemy_id_to_pos with the matching value (state)
-            enemy_key = next(
-                (key for key, value in enemy.enemy_id_to_pos.items() if
-                 value == state or (value[0], value[1]) == (state[0] - 1, state[1])),
-                None)
-
-            if enemy_key is not None and enemy_key not in enemy.enemy_id_removed:
-                # Get the index of the enemy sprite based on the key
-                index = list(enemy.enemy_id_to_pos).index(enemy_key)
-                # Add id enemy removed
-                enemy.enemy_id_removed.append(enemy_key)
-                # Perform actions on the enemy sprite based on the index
-                enemy.enemy_sprite_list[index].kill()
-
-                # Remove the bullet
                 bullet.bullet_id_to_sprite[bullet_id].kill()
                 bullet.bullet_last_pop.append(bullet_id)
                 bullet.bullet_id_to_pos.pop(bullet_id)
